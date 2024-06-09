5cce18a87459c129618e82b5461138b06bad09c9
James Leslie <jamesleslie@posteo.net>
2024-06-09 21:57:28 +0100

    Add hexagon

--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
 import Descartes qualified as D
-import Control.Monad (when)
+import Control.Monad (when, forM_)
+import Data.Complex qualified as C
+import Data.Functor ((<&>))
 
 main :: IO ()
 main = D.withGraphics $ do
-  D.setColour D.Red
+  D.setColour D.Blue
 
   putStrLn "Click twice to draw a line."
   p1 <- D.getPoint
@@ -23,7 +25,12 @@ main = D.withGraphics $ do
 
   D.fillRectangle $ D.rectangle bl tr
 
+  D.setColour D.Green
+  forM_ makeHexagon D.draw
+
+  D.setColour D.Red
   makePath Nothing
+
 
 
 makePath :: Maybe D.Point -> IO ()
@@ -32,3 +39,22 @@ makePath (Just p) = do
   p' <- D.getPoint
   D.draw $ D.lineSeg p p'
   makePath (Just p')
+
+joinPoints :: [D.Point] -> [D.LineSeg]
+joinPoints [] = []
+joinPoints [p] = []
+joinPoints (p1:p2:ps) = D.lineSeg p1 p2 : joinPoints (p2:ps)
+
+makeHexagon :: [D.LineSeg]
+makeHexagon =
+  let
+    scale f = round $ 166 * (f + 1.5)
+
+    -- We use 7 here so that the last point is equal to the first.
+    powers :: [C.Complex Float] =
+      take 7 $ iterate (\ z -> z * C.mkPolar 1 (pi / 3)) (1.0 C.:+ 0)
+
+    points :: [D.Point] =
+      powers <&> \ z -> D.point (scale . C.realPart $ z) (scale . C.imagPart $ z)
+
+  in joinPoints points
